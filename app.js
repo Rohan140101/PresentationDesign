a9db747068fe6d862cda1d9f036852ad38f3632e
Rohan140101 <rohan.kuckian@gmail.com>
2021-05-25 14:14:27 +0530

    Minor Changes

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const ejs = require('ejs')
+const mongoose = require("mongoose");
+const md5 = require('md5');
+// const session = require('express-session');
 
 const app = express()
 app.set('view engine','ejs')
@@ -8,6 +12,14 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('assets'))
 
 const faqs = require(__dirname + "/assets/vendor/json/faqs.json")
+
+const UserSchema = new mongoose.Schema({
+	first_name:String,
+	last_name:String,
+	email:String,
+	
+
+})
 
 
 app.get("/",function(req,res){
@@ -19,7 +31,7 @@ app.get("/pricing",function(req,res){
 })
 
 app.get("/faq",function(req,res){
-	console.log(faqs)
+	// console.log(faqs)
 	res.render("faq",{faqs:faqs})
 })
 
